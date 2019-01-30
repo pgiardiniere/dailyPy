04fea7d922fd4951c1adfcd9653392d4d59ef632
pgiardiniere <pgiardiniere@gmail.com>
2019-01-30 01:48:56 -0500

    added input and comments

--- txtReader.py
+++ txtReader.py
@@ -2,9 +2,16 @@
 import os
 import glob
 
+# get pwd (working dir) and print. OS independent method.
+# could be useful modules if attempting to search in other dirs or recursive search
 pwd = glob.glob(os.getcwd())
+print(pwd)
 
+s = input("enter string to query txt files in current directory for")
+
+#foreach file in pwd 
+#  print contents --- expand to actually take s and search each file for it.
 for file in glob.glob("*.txt"):
   print(file)
 
-print(pwd)
+#import fileinput (multiple streams) or use open() with loops to go into the txt files
