5089d93a13d74c4d651a8b8c9a5d33f9804d63da
pgiardiniere <pgiardiniere@gmail.com>
2019-02-03 20:10:51 -0500

    search partial line, case insensitive IO, other readibility improvements

--- txtReader.py
+++ txtReader.py
@@ -1,5 +1,8 @@
-# get all .txt files in current dir and add them to the list files
 import glob
+import fileinput
+import re
+
+# get all .txt files in current dir and add them to the list files
 files = []
 for file in glob.glob("*.txt"):
     files.append(file)
@@ -7,15 +10,16 @@ print("\nThe files are:\n", files)
 
 #get string to match:
 print("please enter the string you want to match \n")
-s = input()
+s = input().strip().lower()
 print(s, "accepted \n")
 
 # documentation https://docs.python.org/2/library/fileinput.html
-# iterates line-by-line thru each file in the files list, evaluates whole-line string match (less newline, obv)
-#   need fileinput assigned to variable fi in order to access
-#       isn't there a 'this' or 'self' method of self-referncing an objects. might use Super? not sure. revisit.
-import fileinput
+# iterates line-by-line thru each file in the files list, evaluates whole-line string match (less newline)
+
 fi = fileinput.input(files)
 for line in fi:
-    print(s, line.rstrip())
-    if (s == line.rstrip()): print("\t\t MATCH FOUND IN", fi.filename() + ",", "line", fi.filelineno())
+    if (line.lower().find(s) > -1): 
+        print("Match found in: {0}, line number {1}.".format(fi.filename(), fi.filelineno()))
+        print("The line reads: {0}".format(line))
+
+
