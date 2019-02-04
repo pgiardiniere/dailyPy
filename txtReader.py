60de8ffbfb58636f92bfbb1f4b713750b7cf6df6
pgiardiniere <pgiardiniere@gmail.com>
2019-02-03 20:34:08 -0500

    searches large local txt dir by default now

--- txtReader.py
+++ txtReader.py
@@ -2,14 +2,34 @@ import glob
 import fileinput
 import re
 
-# get all .txt files in current dir and add them to the list files
-files = []
-for file in glob.glob("*.txt"):
-    files.append(file)
-print("\nThe files are:\n", files)
+###############
+### TO-DO
+# when working in tech txt notes dir, the output is SUPER long as it prints absolute path of entire txt file. Need to truncate
+# can either 1) cut the output from the strings somehow, 
+# or 2) do something to 'switch' execution actually into that dir and then execute
+# 
+# that would reduce redundant code - i.e. it would work the same in pwd or in another remote dir
+# also opens up the possibility for arbitrary dir search
+###############
+
+# confirm user is searching the standard tech txt notes dir
+print("Hit enter to search your standard tech txt notes dir. Insert any string to search current dir instead\n")
+pwd = input()
+
+# if Return inputted, get all .txt files in tech txt notes dir and add them to the list files. Else, just use current dir
+if pwd == '':
+    files = []
+    for file in glob.glob(r"C:\\Users\\Pete\\Google Drive\\Tech txt notes\\*.txt"):
+        files.append(file)
+    print("\nThe files are:\n", files)
+else:
+    files = []
+    for file in glob.glob("*.txt"):
+        files.append(file)
+    print("\nThe files are:\n", files)
 
 #get string to match:
-print("please enter the string you want to match \n")
+print("please enter the string you want to match\n")
 s = input().strip().lower()
 print(s, "accepted \n")
 
