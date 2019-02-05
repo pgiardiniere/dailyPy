ca431bbb67f3cebe9f8b67874d000701d14ced44
pgiardiniere <pgiardiniere@gmail.com>
2019-02-05 18:05:47 -0500

    changed repo hierarchy - including backlog of outside exercises

---
+++ misc/whirlwindTourOfPython/wt13-modulesPkgs.py
@@ -0,0 +1,20 @@
+### Explicit module import
+import math
+print(math.cos(math.pi))
+
+### explicit module import by alias
+import numpy as np
+print(np.cos(np.pi))
+
+### explicit import of module contents (to local namespace)
+from math import cos, pi
+print(cos(pi))
+
+### implicit import of module contents (to local namespace)
+### commented belowto squelch errors
+#   from math import *  
+#   print(sin(pi) ** 2 + cos(pi) ** 2)
+
+    # the above is not a recommended method of import. Per the 'problems' in VSCode console,
+    # there are 50 funcs in math module imported to local namespace, of which only 3 are used
+    # if I had functions before this import of the same name, they would be implicitly overwritten
