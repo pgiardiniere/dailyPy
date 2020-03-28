7270ff2023f153de0d32ef6181486683f0d9723e
pgiardiniere <pgiardiniere@gmail.com>
2020-03-28 18:22:28 -0400

    recursive overflow issue, verified changes persisting. write output to file

--- sudoku-plus/sudoku.py
+++ sudoku-plus/sudoku.py
@@ -1,18 +1,19 @@
 # This is a Python port of my Java sudoku solver written for 2168: Data Structs
 # Against my better judgment, we'll do this with plain old Lists first time around.
+import sys
 
 class Sudoku:
     def __init__(self):
         self.board = [
             [9, 6, 0, 0, 1, 0, 2, 0, 8],
             [0, 8, 0, 5, 0, 0, 1, 9, 4],
-            [0, 0, 0, 0, 2, 9, 3, 4, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            [0, 0, 5, 2, 0, 0, 0, 0, 6],
+            [0, 0, 0, 0, 2, 9, 3, 4, 0],            
+            [0, 0, 0, 8, 0, 6, 0, 0, 0],
+            [0, 9, 2, 4, 3, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 7, 8, 0],
+            [6, 7, 9, 0, 0, 2, 0, 1, 0],
+            [5, 0, 8, 0, 4, 0, 0, 7, 3]
         ]
 
     def board_print(self):
@@ -22,26 +23,68 @@ class Sudoku:
                 if (j == 8): print()
         print()
 
-    def pretty_print(self):
+    def pretty_print(self, output):
         for i in range(9):
-            if (i > 0 and i % 3 == 0):
-                print('---------------------')
+            if i > 0 and i % 3 == 0:
+                print('---------------------', file=output)
             for j in range(9):
                 if (j > 0 and j % 3 == 0):
-                    print('|', end=' ')
-                print(self.board[i][j], end=' ')
+                    print('|', end=' ', file=output)
+                print(self.board[i][j], end=' ', file=output)
                 if (j == 8): 
-                    print()
-        print()
-    
+                    print(file=output)
+        print(file=output)
+
+
+# def find_next(board):
+#     for i in range(9):
+#         for j in range(9):
+#             if not board[i][j]: return (i,j)
+#     return (-1,-1)
+
+
+# def valid_move(board, row, col, val):
+#     for j in range(9):
+#         if board[row][j] == val:
+#             return False
+
+#     for i in range(9):
+#         if board[i][col] == val:
+#             return False
+
+#     tile = (row - row % 3, col - col % 3)
+#     for i in range(tile[0], tile[0] + 3):
+#         for j in range(tile[1], tile[1] + 3):
+#             if board[i][j] == val:
+#                 return False
+
+#     return True
+
+# def solve(board):
+#     row, col = find_next(board)
+#     if (row == -1): return True
+
+#     for val in range(1, 10):
+#         if valid_move(board, row, col, val):
+#             board[row][col] = val
+
+#         if (solve(board)): 
+#             return True
+#         else: 
+#             board[row][col] = 0
+#             return False
+
+def change_persists(board):
+    board[0][0] = 2003
 
 def main():
     sudoku = Sudoku()
+    output = open('output.txt', 'w')
 
-    sudoku.board_print()
-    sudoku.pretty_print()
+    sudoku.pretty_print(output)
+    change_persists(sudoku.board)
+    sudoku.pretty_print(output)
 
-    sudoku.board[0][0] = 'F'
-    sudoku.pretty_print()
+    output.close()
 
 main()
