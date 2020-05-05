f3c2d91f547ae66afc07a4e7cda28cba1a6496e9
pgiardiniere <pgiardiniere@gmail.com>
2020-05-05 12:57:07 -0400

    finish module 6, @property decorators

--- corey-schafer-OOP/6-property-decorators.py
+++ corey-schafer-OOP/6-property-decorators.py
@@ -7,40 +7,48 @@ class Employee:
 
     @property
     def email(self):
-        return '{}.{}'.format(self.first, self.last)
+        return '{}.{}@email.com'.format(self.first, self.last)
 
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     @fullname.setter
     def fullname(self, name):
         first, last = name.split(" ")
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        self.first = None
+        self.last = None
 
 
 # Problem: At initialization, create email. What about name updates?
+# Solution: Create a Getter using the @property decorator for email method.
 emp_1 = Employee('John', 'Smith')
 print(emp_1.first)
 print(emp_1.email)
-print(emp_1.fullname(), end='\n\n')
+print(emp_1.fullname, end='\n\n')
 
 emp_1.first = 'Jim'
 print(emp_1.first)
 print(emp_1.email)
-print(emp_1.fullname(), end='\n\n')
+print(emp_1.fullname, end='\n\n')
 
-# Solution: Create a Setter using the @property decorator.
-# With @property we define a method, but access like an attribute.
+# I.e. with @property we define a method, but access like an attribute.
 
 
 # Problem: Assign first and last name of an Employee via fullname string
-#   print(emp_1.fullname())
-#   emp_1.fullname = 'Corey Schafer'
-#   print(emp_1.fullname)
-#       In presenter's notes, that throws an error
-#       For me that overwrites the fullname() function with fullname() string
-print(emp_1.fullname())
+# Solution: Create a Setter using the @function.setter decorator.
+
+print(emp_1.fullname)
 emp_1.fullname = 'Corey Schafer'
-print(emp_1.fullname())
+print(emp_1.fullname, end='\n\n')
 
 
-# Solution: Create a Getter using the @__ decorator.
+# The @function.deleter deleter behaves as you'd expect at this point.
+print(emp_1.fullname)
+del emp_1.fullname
+print(emp_1.fullname)
