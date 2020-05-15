870ace3d42f31c3170b90bd83cdd7f6b9aa55e41
pgiardiniere <pgiardiniere@gmail.com>
2020-05-14 23:15:50 -0400

    delete() added

--- data-structs/CCI-Gayle/single-linked-list.py
+++ data-structs/CCI-Gayle/single-linked-list.py
@@ -15,7 +15,21 @@ class Node:
         while node.next is not None:
             print(node.data)
             node = node.next
-        print(node.data)
+        print(node.data, end='\n\n')
+
+    def delete(self, dat):
+        node = self
+
+        if (node.data == dat):
+            return node.next
+
+        while node.next is not None:
+            if node.next.data == dat:
+                node.next = node.next.next
+                return self
+            node = node.next
+
+        return self
 
 
 if __name__ == '__main__':
@@ -23,3 +37,12 @@ if __name__ == '__main__':
     n.add('second')
     n.add('third')
     n.print()
+
+    n = n.delete('first')
+    n.print()
+
+    n = n.delete('second')
+    n.print()
+
+    # n = n.delete('third')
+    # n.print()
