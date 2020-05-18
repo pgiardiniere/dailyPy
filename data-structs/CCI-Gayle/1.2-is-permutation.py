7dc8b9ef5d253146f7a741f057babf9e28081d73
pgiardiniere <pgiardiniere@gmail.com>
2020-05-17 23:36:31 -0400

    sorting solution

--- data-structs/CCI-Gayle/1.2-is-permutation.py
+++ data-structs/CCI-Gayle/1.2-is-permutation.py
@@ -7,15 +7,22 @@ def is_permutation(s1, s2):
     if len(s2) > len(s1):
         return False
 
-    for c2 in s2:
-        if c2 not in s1:
+    for c in s2:
+        if c not in s1:
             return False
 
     return True
 
+# cheating by not implementing my own sorting algorithm, but it'll do.
+def is_strict_permutation(s1, s2):
+    if len(s2) != len(s1):
+        return False
 
-# def is_strict_permutation(s1, s2):
-    
+    s1_sorted = sorted(s1)
+    s2_sorted = sorted(s2)
+
+    if s1_sorted == s2_sorted:
+        return True
 
 
 if __name__ == '__main__':
@@ -34,3 +41,21 @@ if __name__ == '__main__':
     """
 
     # Yeah, as I thought. Playing with definition.
+
+    """
+    Hint 2:
+    There is one solution that is O(N log N) time.
+    Another solution uses more space, but is O(N) time.
+    """
+
+    # Okay, so the O(N log N) solution implies an in-place sort on string
+
+    # And the O(N) solution implies use of a (two?) hashtable(s).
+    # Actually, we could have 2 strings of equal length and hashtable,
+    # but with different num instances of chars hashing to same thing.
+
+    # If I'm using a strict hashtable and not a hashset, then it will work.
+
+    s1 = 'hello'
+    s2 = 'olleh'
+    print(is_strict_permutation(s1, s2))
