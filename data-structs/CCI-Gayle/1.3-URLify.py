facb5280f86416f17c35ff1dd4bb6704849b2494
pgiardiniere <pgiardiniere@gmail.com>
2020-05-20 23:57:29 -0400

    in-place urlify

--- data-structs/CCI-Gayle/1.3-URLify.py
+++ data-structs/CCI-Gayle/1.3-URLify.py
@@ -35,6 +35,29 @@ def make_url_from_arr(L):
     return L2
 
 
+# NP char array function, in-place. Pass in some padded array
+def make_url_in_place(L):
+    count = 0
+    for i, c in enumerate(L):
+        if c == ' ':
+            count += 1
+    i_adj = count * 3
+
+    i = L.size - 1
+    j = i - i_adj
+    while (j > -1):
+        if L[j] == ' ':
+            L[i] = '0'
+            L[i-1] = '2'
+            L[i-2] = '%'
+            i -= 2
+        else:
+            L[i] = L[j]
+        i -= 1
+        j -= 1
+    return L
+
+
 # URL-ify a string.
 s = 'one space'
 print(s)
@@ -61,10 +84,21 @@ for i, c in enumerate(s):
     else:
         L = np.append(L, c)
 
-
 print(L)
 L = make_url_from_arr(L)
 print(L, end='\n\n')
+
+
+# URL-ify a NumPy char array in-place
+s = 'one space'
+for i, c in enumerate(s):
+    L = np.array(c) if i == 0 else np.append(L, c)
+L = np.append(L, ['0', '0', '0'])
+
+print(L)
+L = make_url_in_place(L)
+print(L, end='\n\n')
+
 
 
 # Hint #1:
@@ -81,3 +115,19 @@ You might find you need to know num spaces. Try just counting them.
 # Alright, that was not the best idea.
 
 # Figure out how NumPy does in-place reassignment of array size...
+
+# AH HAH
+# https://numpy.org/devdocs/user/whatisnumpy.html
+
+# NumPy arrays are fixed in size at their creation.
+# If you need more elemenents than were in the original array,
+# you'll have to create a new array and put the stuff in there.
+
+# Oh wait.
+# If you're reallocating an array in C/Java, you're doing the same thing.
+
+#   define array : len(arrray) = 2 * length of old array
+#   iterate through old array, copying elements in to new
+#   use the additional space for extra stuffs.
+
+# So I guess we're meant to assume that the array is padded with nulls / 0s
